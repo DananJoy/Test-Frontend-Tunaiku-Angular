c007e7c94b6471edf5076115cffea3ca37d23227
Joy <putradananjoyo@gmail.com>
2017-05-11 23:36:12 +0700

    fixed inputbox for mobile devices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProfileComponent } from './profile/profile.component';
+
+const FirebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+}
+
+let routes: Routes = [
+  { path:'', pathMatch:'full', redirectTo: 'home'},
+  { path:'home', component: HomeComponent } ,
+  { path:'about', component: AboutComponent },
+  { path:'profile', component: ProfileComponent }
+]
 
 @NgModule({
   declarations: [
@@ -18,7 +36,9 @@ import { ProfileComponent } from './profile/profile.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(FirebaseConfig),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
